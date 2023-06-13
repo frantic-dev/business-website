8e3dd9d6365ba93934cc8a9ba9e78a38acd21651
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2023-06-13 17:36:26 +0100

    fix carousel

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-let slide = document.querySelector('#slide-wrapper');
-let rightArrow = document.querySelector('#right-arrow');
-let leftArrow = document.querySelector('#left-arrow');
-let firstSlide = document.querySelector('#first-slide');
-let scndSlide = document.querySelector('#scnd-slide');
+let slide = document.querySelector("#slide-wrapper");
+let rightArrow = document.querySelector("#right-arrow");
+let leftArrow = document.querySelector("#left-arrow");
+let firstSlide = document.querySelector("#first-slide");
+let scndSlide = document.querySelector("#scnd-slide");
+let frstSlide = true;
 function nextSlide() {
-  let frstSlide = true;
-  if(frstSlide) {
-    slide.className = "transition";
+  if (frstSlide) {
+    slide.className = "next-transition";
+    frstSlide = false;
   }
 }
-rightArrow.addEventListener('click',nextSlide)
+function prvSlide() {
+  if (!frstSlide) {
+    slide.className = "prv-transition";
+    frstSlide = true
+  }
+}
+rightArrow.addEventListener("click", nextSlide)
+leftArrow.addEventListener("click", prvSlide)
